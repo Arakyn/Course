e9e8424ae2da30cc1fe402df66be5b1d4bc7eb26
Arakyn <wizzyt2222@gmail.com>
2022-09-20 16:08:40 +0530

    fuke chabe

--- training/excercises/01HelloWorld/var.go
+++ training/excercises/01HelloWorld/var.go
@@ -7,7 +7,7 @@ var z int  // declared a variable with identifier Z which is of type int
 // not assigning a value will lead to it having a value of Zero of its type
 
 func main() {
-	fmt.Println("Hello Worldcxs ")
+	fmt.Println("Hello Worldcxdsss ")
 	x := 42 // short term declaration, it can only be used in this func main()
 	fmt.Println(x)
 	fmt.Println(y)
