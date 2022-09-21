3305d04700b225d677becb5be511ed5d3a44863e
Arakyn <wizzyt2222@gmail.com>
2022-09-21 12:31:29 +0530

    nice

--- training/excercises/33Documentation/01-go-doc/main.go
+++ training/excercises/33Documentation/01-go-doc/main.go
@@ -1,5 +1,12 @@
-package main
+// package math takes in  integers and adds them
+package Maths
 
-func main() {
+// func maths takes a int and returns a added value idk mai to itnerne t hui
+func Maths(x ...int) int {
+	sum := 0
+	for _, v := range x {
+		sum += v
+	}
+	return sum
 
 }
