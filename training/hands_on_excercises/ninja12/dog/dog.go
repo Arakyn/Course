c808d0464ba90b2fe55b8d7982cd47f17500afbb
Arakyn <wizzyt2222@gmail.com>
2022-09-23 12:03:24 +0530

    i am doing great

---
+++ training/hands_on_excercises/ninja12/dog/dog.go
@@ -0,0 +1,9 @@
+// Package Dog takes in Human years and converts them into dog years.
+package dog
+
+// func convert takes in a integer which is human age and returns the converted years.
+func Convert(x int) int {
+	sum := x * 7
+	return sum
+
+}
---
+++ training/hands_on_excercises/ninja12/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/github.com/Arakyn/Course/training/hands_on_excercises/ninja12/dog"
+)
+
+func main() {
+	var x int
+	fmt.Println("Lets convert the ages")
+	fmt.Scanln(&x)
+	fmt.Println("Your Age converted into Doc Years. : ", dog.Convert(x))
+
+}
