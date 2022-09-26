56005325254a86adac5bf8547cb4942874a258d5
Arakyn <wizzyt2222@gmail.com>
2022-09-26 22:52:32 +0530

    yes

---
+++ training/hands_on_excercises/ninja13/01-hands/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+type canine struct {
+	name string
+	age  int
+}
+
+func main() {
+	fido := canine{
+		name: "KUTTA",
+		age:  7,
+	}
+	fmt.Println(doggy.Years())
+}
