a50af47b1fda2adc8282f9a1466f3f487cd12d4d
Arakyn <wizzyt2222@gmail.com>
2022-09-27 14:55:27 +0530

    yes

---
+++ training/hands_on_excercises/ninja13/02-hands/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/github.com/Arakyn/Course/training/hands_on_excercises/ninja13/02-hands/quote"
+	"github.com/github.com/Arakyn/Course/training/hands_on_excercises/ninja13/02-hands/word"
+)
+
+func main() {
+	fmt.Println(word.Count(quote.SunAlso))
+	for k, v := range word.UseCount(quote.SunAlso) {
+		fmt.Println(v, k)
+	}
+}
---
+++ training/hands_on_excercises/ninja13/02-hands/word/main_test.go
@@ -0,0 +1,14 @@
+package word
+
+import (
+	"testing"
+
+	"github.com/github.com/Arakyn/Course/training/hands_on_excercises/ninja13/02-hands/word"
+)
+
+func TestCount(t *testing.T) {
+
+	xs := `Aman Aman Aman Joker Joker Joker Taliban`
+	word.Count()
+
+}
