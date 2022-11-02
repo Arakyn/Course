4bc0dbb80c0e22e37a1b813b50db73527c8a5908
Arakyn <wizzyt2222@gmail.com>
2022-11-02 17:33:20 +0530

    vdfbdfbz

--- training/excercises/30Channels/08-FanOut/main1.go
+++ training/excercises/30Channels/08-FanOut/main1.go
@@ -10,7 +10,7 @@ func main() {
 	ye := make(chan int)
 	even := make(chan int)
 
-	go send(ye)
+	send(ye)
 	go recieve(even, ye)
 	for v := range even {
 		fmt.Println("From Even", v)
@@ -19,21 +19,23 @@ func main() {
 	fmt.Println("Abou to exit")
 }
 func send(fo chan<- int) {
-	for i := 0; i <= 100; i++ {
+	go func() {
+		for i := 0; i <= 100; i++ {
 
-		fo <- i
-	}
-	close(fo)
+			fo <- i
+		}
+		close(fo)
+	}()
 }
 func recieve(e chan int, fo chan int) {
 	var wg sync.WaitGroup
 	for v := range fo {
-		wg.Add(1)
+
 		go func(v int) {
+			wg.Add(1)
 			e <- v
 			fmt.Println("Number of GORoutines: ", runtime.NumGoroutine())
 			wg.Done()
-
 		}(v)
 
 	}
